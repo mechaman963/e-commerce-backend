6ca65c9bc99d3e5b6e23e900c9aea19dd6ec8bb1
mechaman963 <mecha.man.963@gmail.com>
2025-09-12 08:26:21 +0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001', 'http://localhost:8080', 'http://127.0.0.1:8080'],
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
